c3837ad56f0506ff3e5b0dfdeed0b81f899f7e52
caiotelesz <caioteles1267@gmail.com>
2024-06-01 13:58:02 -0300

    Feito a metade da home page

--- src/home/index.js
+++ src/home/index.js
@@ -1,18 +1,60 @@
-import CardHeader from '../components/CardHeader';
-import CardFooter from '../components/CardFooter';
-import './index.scss';
+import CardHeader from "../components/CardHeader";
+import CardFooter from "../components/CardFooter";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+import { faDog } from "@fortawesome/free-solid-svg-icons";
+
+import "./index.scss";
 
 export default function Home() {
-  return(
-    <div>
+  return (
+    <section className="section1">
       <CardHeader />
+      <div className="home1">
+        <h1>Estamos Aqui Para Cuidar do Seu Pet</h1>
+      </div>
 
-      <section>
-        <p> Testando!</p>
-      </section>
+      <div className="home2">
+        <h1>Tudo que o seu pet precisa.</h1>
+        <p>
+          atendimento veterinário - cirurgia geral - Exames laboratoriais -
+          vacinas importadas Farmácia Veterinária - Banho e tosa - Taxi Dog
+          (Serviço de leva e traz)
+        </p>
+        <div>
+          <div>
+            <FontAwesomeIcon icon={faEnvelopeOpen} className="icon-Home" />
+            <h2>Nossa História</h2>
+            <p>
+              A clínica veterinária Amicitia, é um ponto de referência na
+              comunidade. Com uma equipe dedicada, oferece serviços de qualidade
+              e promove a educação sobre cuidados com animais. É um centro de
+              cuidado e compaixão, fortalecendo os laços entre humanos e animais
+              na cidade.
+            </p>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faDog} className="icon-Home" />
+            <h2>O que fazemos?</h2>
+            <p>
+              Nosso objetivo é entregar serviços veterinários de alta qualidade
+              para cães e gatos, incluindo:
+              <ul>
+                <li>Clínica geral</li>
+                <li>Cirurgias</li>
+                <li>Exames laboratoriais</li>
+                <li>Vacinação</li>
+                <li>Estética Animal</li>
+                <li>Farmácia Veterinária</li>
+                <li>Pet-shop.</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
 
       <CardFooter />
-
-    </div>
+    </section>
   );
 }
