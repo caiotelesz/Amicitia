85db4bd0f2efd601bbb10fcef0022fb1d67b728c
caiotelesz <caioteles1267@gmail.com>
2024-06-09 21:10:39 -0300

    Ajustando trabalhe conosco

--- src/pages/trabalhe-conosco/index.js
+++ src/pages/trabalhe-conosco/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 import CardFooter from "../../components/CardFooter";
 import CardHeader from "../../components/CardHeader";
 import "./index.scss";
@@ -6,12 +6,23 @@ import "./index.scss";
 export default function TrabalheConosco() {
   const fileInputRef = useRef(null);
 
+  const [formData, setFormData] = useState({
+    name: "",
+    dateOfBirth: "",
+    email: "",
+    phone: "",
+    address: "",
+    fileName: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
   const fileClick = () => {
     fileInputRef.current.click();
   };
 
   const fileChange = (event) => {
-    const fileName = event.target.files[0] ? event.target.files[0].name : '';
+    const fileName = event.target.files[0] ? event.target.files[0].name : "";
     setFormData({ ...formData, fileName });
   };
 
@@ -20,114 +31,130 @@ export default function TrabalheConosco() {
     setFormData({ ...formData, [id]: value });
   };
 
-  const enviar = () => {
-    alert('Currículo enviado!');
+  const enviar = (event) => {
+    event.preventDefault();
+    const { name, dateOfBirth, email, phone, address, fileName } = formData;
+
+    if (!name || !dateOfBirth || !email || !phone || !address || !fileName) {
+      setErrorMessage("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    alert("Currículo enviado!");
     setFormData({
-      name: '',
-      dateOfBirth: '',
-      email: '',
-      phone: '',
-      address: '',
-      fileName: ''
+      name: "",
+      dateOfBirth: "",
+      email: "",
+      phone: "",
+      address: "",
+      fileName: "",
     });
     fileInputRef.current.value = null;
+    setErrorMessage("");
   };
 
-  const [formData, setFormData] = useState({
-    name: '',
-    dateOfBirth: '',
-    email: '',
-    phone: '',
-    address: '',
-    fileName: ''
-  });
-
   return (
-    <section className="section-trabalharConosco">
+    <>
       <CardHeader />
+      <section className="section-trabalharConosco">
+        <div className="card-trabalharConosco">
+          <h1>Gostaria de fazer parte da nossa equipe?</h1>
+          
+          <div className="center">
+          <h2>Envie suas informações agora mesmo!</h2>
+          </div>
 
-      <div className="card-trabalharConosco">
-        <h1>Gostaria de fazer parte da nossa equipe?</h1>
-        <h2>Envie suas informações agora mesmo!</h2>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-        <div className="input-group">
-          <label htmlFor="name">Nome</label>
-          <input
-            id="name"
-            type="text"
-            placeholder="Nome"
-            value={formData.name}
-            onChange={buttonChange}
-          />
+          <form onSubmit={enviar}>
+            <div className="input-group">
+              <label htmlFor="name">Nome</label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Nome"
+                value={formData.name}
+                onChange={buttonChange}
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="dateOfBirth">Data de Nascimento</label>
+              <input
+                id="dateOfBirth"
+                type="date"
+                placeholder="dd/mm/yyyy"
+                value={formData.dateOfBirth}
+                onChange={buttonChange}
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="email">E-mail</label>
+              <input
+                id="email"
+                type="email"
+                placeholder="E-mail"
+                value={formData.email}
+                onChange={buttonChange}
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="phone">Telefone</label>
+              <input
+                id="phone"
+                type="tel"
+                placeholder="Telefone"
+                value={formData.phone}
+                onChange={buttonChange}
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="address">Endereço</label>
+              <input
+                id="address"
+                type="text"
+                placeholder="Endereço"
+                value={formData.address}
+                onChange={buttonChange}
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="file-upload">Currículo</label>
+              <input
+                id="file-name"
+                type="text"
+                placeholder="Clique aqui"
+                readOnly
+                value={formData.fileName}
+                onClick={fileClick}
+                required
+              />
+              <input
+                id="file-upload"
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={fileChange}
+                required
+              />
+            </div>
+
+            <div className="center">
+            <button type="submit">ENVIAR</button>
+            </div>
+          </form>
         </div>
-
-        <div className="input-group">
-          <label htmlFor="dateOfBirth">Data de Nascimento</label>
-          <input
-            id="dateOfBirth"
-            type="date"
-            placeholder="dd/mm/yyyy"
-            value={formData.dateOfBirth}
-            onChange={buttonChange}
-          />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="email">E-mail</label>
-          <input
-            id="email"
-            type="text"
-            placeholder="E-mail"
-            value={formData.email}
-            onChange={buttonChange}
-          />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="phone">Telefone</label>
-          <input
-            id="phone"
-            type="text"
-            placeholder="Telefone"
-            value={formData.phone}
-            onChange={buttonChange}
-          />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="address">Endereço</label>
-          <input
-            id="address"
-            type="text"
-            placeholder="Endereço"
-            value={formData.address}
-            onChange={buttonChange}
-          />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="file-upload">Currículo</label>
-          <input
-            id="file-name"
-            type="text"
-            placeholder="Currículo"
-            readOnly
-            value={formData.fileName}
-            onClick={fileClick}
-          />
-          <input
-            id="file-upload"
-            type="file"
-            ref={fileInputRef}
-            style={{ display: 'none' }}
-            onChange={fileChange}
-          />
-        </div>
-
-        <button onClick={enviar}>ENVIAR</button>
-      </div>
-
+      </section>
       <CardFooter />
-    </section>
+    </>
   );
 }
